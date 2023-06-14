3e81e3bf7ab1495d9ea49bf1ffec91d0ef345804
k1m2njun <k1m2njun@naver.com>
2023-06-14 14:28:56 +0900

    Config #1 - spring configuration
    기초 설정
    루트 패키지 이하에 빈이미지 스캔

---
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package com.fastcampus.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fastcampus.springpractice")
+@Configuration // 스프링 전용 config가 된다.
+public class Config {
+}
